34e2a294cc5d1147a8238426bb88084d19edc18b
JaeSeong.Song <zzyzigy@gmail.com>
2023-03-02 15:11:05 +0900

    recipe-app v3b : 메인 이미지 추가

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,13 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import Instructions from "./Instructions";
+import Logo from "../../public/images/smile.png";
 
 const Recipe = ({ name, ingredients, steps }) => {
     let fname = name.toLowerCase().replace(/ /g, '-');
     return (
         <React.StrictMode>
+            <div><img src={Logo} /></div>
             <section id={fname}>
                 <h1>{name}</h1>
                 <div><img src={`./images/${fname}.png`} /></div>
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,19 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: "source-map",
     entry: "./src/index.js",   // 진입(시작) 파일 
     output: {
         path: path.join(__dirname, "dist"),   // 번들파일 저장위치
         filename: "app.bundle.js"   // 기본파일명 : main.js
     },
     module: {   // 각 파일에 대한 세부적인 번들링 작업 정의
-        rules: [{ test: /\.js$/,   // .js 로 끝나는 파일에 대한 규칙 정의
-            exclude: /node_modules/, loader: "babel-loader" }]
+        rules: [
+            { test: /\.js$/,   // .js 로 끝나는 파일에 대한 규칙 정의
+                exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.(png|jpe?g|gif|svg|ico)$/i, // .js 로 끝나는 파일에 대한 규칙 정의
+                use: [ { loader: "file-loader", }, ], },
+        ]
     },
     devServer: {  // 서버 구동시 관련 내용 설정
         static: path.join(__dirname, 'dist'),
