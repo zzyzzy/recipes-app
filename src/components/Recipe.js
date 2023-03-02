4f0e2ac6702d7b4159e8c65477b7d00fbeb8e6a5
JaeSeong.Song <zzyzigy@gmail.com>
2023-03-02 13:16:41 +0900

    recipe-app v3 : 요리 이미지 추가

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,14 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
-import Ingredient from "./Ingredient";
 import Instructions from "./Instructions";
 
 const Recipe = ({ name, ingredients, steps }) => {
+    let fname = name.toLowerCase().replace(/ /g, '-');
     return (
         <React.StrictMode>
-            <section id={name.toLowerCase().replace(/ /g, '-')}>
+            <section id={fname}>
                 <h1>{name}</h1>
+                <div><img src={`./images/${fname}.png`} /></div>
                 <IngredientsList list={ingredients} />
                 <Instructions title="요리 방법" steps={steps} />
             </section>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Menu from "./components/Menu";
-import data from "./data/recipes_ko.json";
-// import data from "./data/recipes.json";
+// import data from "./data/recipes_ko.json";
+import data from "./data/recipes.json";
 
 ReactDOM.render(
     <React.StrictMode>
